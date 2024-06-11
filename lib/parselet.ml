1a73fe86a5146fd62a4173cb566232714622067b
Qexat <43090614+qexat@users.noreply.github.com>
2024-06-11 21:47:25 +0200

    idk what i am doing actually

--- lib/parselet.ml
+++ lib/parselet.ml
@@ -3,3 +3,15 @@ type 'parser prefix_parselet_func = parser:'parser -> token:Token.token -> Ast.e
 
 type 'parser infix_parselet_func =
   parser:'parser -> left:Ast.expr -> token:Token.token -> Ast.expr
+
+module type ParserType = sig
+  type t
+
+  val parse_expression : precedence:int option -> t -> Ast.expr
+end
+
+module type InfixParselet = functor (Parser : ParserType) -> sig
+  val precedence : int
+  val is_right_associative : bool
+  val parse : parser:Parser.t -> token:Token.token -> Ast.expr
+end
--- lib/parselet_impls.ml
+++ lib/parselet_impls.ml
@@ -1,6 +1,6 @@
 let literal_parselet ~parser:_ ~token = Ast.LiteralExpr { token }
 
-let binary_op_parselet ~precedence ~is_right_associative ~parser ~left ~token =
+let binary_op_parselet_ ~precedence ~is_right_associative ~parser ~left ~token =
   let right =
     Parser.parse_expression
       parser
@@ -8,4 +8,24 @@ let binary_op_parselet ~precedence ~is_right_associative ~parser ~left ~token =
         (Some (Precedence.precedence_to_enum precedence - Bool.to_int is_right_associative))
   in
   Ast.BinaryOpExpr { operator = Token_type.unwrap_binary_op token.Token.typ; left; right }
+;;
+
+let binary_op_parselet ~precedence ~is_right_associative =
+  (module struct
+    let precedence = precedence
+    let is_right_associative = is_right_associative
+
+    let parse parser token =
+      let right =
+        Parser.parse_expression
+          parser
+          ?precedence:
+            (Some
+               (Precedence.precedence_to_enum precedence
+                - Bool.to_int is_right_associative))
+      in
+      Ast.BinaryOpExpr
+        { operator = Token_type.unwrap_binary_op token.Token.typ; left; right }
+    ;;
+  end)
 ;;
